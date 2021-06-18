987385fad3cdd6a1a1950b391d9fb88c8cb20fd2
David <david-andero.lellep@khk.ee>
2021-06-18 15:05:50 +0300

    Arvestustoo jaoks tehtud muudatused

--- public/index.php
+++ public/index.php
@@ -4,26 +4,33 @@ use App\Service\Templating;
 use DI\Container;
 use Slim\Factory\AppFactory;
 require('../vendor/autoload.php');
+
 // register services
 $container = new Container();
+
 $container->set('db', function(){
 	return DatabaseFactory::create();
 });
+
 $container->set('templating', function() {
+
+
     return new Templating;
 });
+
 AppFactory::setContainer($container);
 // initialise application
 $app = AppFactory::create();
+
 // define page routes
 $app->get('/', '\App\Controller\DefaultController:homepage');
-$app->get('/admin', '\App\Controller\AdminController:view');
-$app->any('/admin/create', '\App\Controller\AdminController:create');
-$app->any('/admin/{id}', '\App\Controller\AdminController:edit');
+$app->get('/admin/article', '\App\Controller\ArticleAdminController:view');
+$app->any('/admin/article/create', '\App\Controller\ArticleAdminController:create');
+$app->any('/admin/article/{id}', '\App\Controller\ArticleAdminController:edit');
 $app->get('/article/{slug}', '\App\Controller\ArticleController:view');
 $app->get('/author/{id}', '\App\Controller\AuthorController:author');
 $app->get('/tags', '\App\Controller\TagController:view');
 $app->get('/tag/{id}', '\App\Controller\TagController:tag');
 
 // finish
-$app->run();
+$app->run(); 
